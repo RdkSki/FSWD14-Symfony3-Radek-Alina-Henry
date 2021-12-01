b4d45c98e76b1d061a753acdf876dded521a1ca1
Henry-NS-2021 <h.ngosytchev@gmail.com>
2021-12-01 15:05:54 +0100

    updated db file

--- todo.sql
+++ todo.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 01, 2021 at 11:26 AM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.12
+-- Erstellungszeit: 01. Dez 2021 um 15:05
+-- Server-Version: 10.4.21-MariaDB
+-- PHP-Version: 8.0.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `todo`
+-- Datenbank: `todo`
 --
 CREATE DATABASE IF NOT EXISTS `todo` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `todo`;
@@ -26,7 +26,7 @@ USE `todo`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `doctrine_migration_versions`
+-- Tabellenstruktur für Tabelle `doctrine_migration_versions`
 --
 
 CREATE TABLE `doctrine_migration_versions` (
@@ -36,16 +36,37 @@ CREATE TABLE `doctrine_migration_versions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `doctrine_migration_versions`
+-- Daten für Tabelle `doctrine_migration_versions`
 --
 
 INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
-('DoctrineMigrations\\Version20211201094215', '2021-12-01 10:42:42', 61);
+('DoctrineMigrations\\Version20211201094215', '2021-12-01 10:42:42', 61),
+('DoctrineMigrations\\Version20211201105147', '2021-12-01 11:52:17', 137),
+('DoctrineMigrations\\Version20211201132910', '2021-12-01 14:29:42', 57);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `todo`
+-- Tabellenstruktur für Tabelle `status`
+--
+
+CREATE TABLE `status` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Daten für Tabelle `status`
+--
+
+INSERT INTO `status` (`id`, `name`) VALUES
+(1, 'Done'),
+(2, 'Not Done');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `todo`
 --
 
 CREATE TABLE `todo` (
@@ -55,42 +76,70 @@ CREATE TABLE `todo` (
   `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `priority` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `due_date` datetime NOT NULL,
-  `create_date` datetime NOT NULL
+  `create_date` datetime NOT NULL,
+  `fk_status_id` int(11) DEFAULT NULL,
+  `picture_url` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `todo`
+-- Daten für Tabelle `todo`
 --
 
-INSERT INTO `todo` (`id`, `name`, `category`, `description`, `priority`, `due_date`, `create_date`) VALUES
-(1, 'walk', 'sport', 'walk for one hour in Schönbrunn', 'Normal', '2020-04-14 00:00:00', '2020-04-01 00:00:00'),
-(2, 'Gym', 'sport', 'Go to gym for 2 hours', 'High', '2020-05-14 00:00:00', '2020-04-01 00:00:00');
+INSERT INTO `todo` (`id`, `name`, `category`, `description`, `priority`, `due_date`, `create_date`, `fk_status_id`, `picture_url`) VALUES
+(1, 'walk', 'sport', 'walk for one hour in Schönbrunn', 'Normal', '2020-04-14 00:00:00', '2020-04-01 00:00:00', 2, NULL),
+(2, 'Gym', 'sport', 'Go to gym for 2 hours', 'High', '2020-05-14 00:00:00', '2020-04-01 00:00:00', 1, NULL),
+(3, 'Test', 'Junk Food', 'Miaaaam!', 'High', '2022-03-07 04:00:00', '2021-12-01 12:01:18', 2, NULL),
+(4, 'Go', 'To', 'Hell', 'Low', '2016-01-01 00:00:00', '2021-12-01 14:19:17', 1, NULL),
+(5, 'Test2', 'test', 'Test2', 'Low', '2016-01-01 00:00:00', '2021-12-01 14:37:21', 1, 'Vegeta-DBS-61a77a9168f42.jpg');
 
 --
--- Indexes for dumped tables
+-- Indizes der exportierten Tabellen
 --
 
 --
--- Indexes for table `doctrine_migration_versions`
+-- Indizes für die Tabelle `doctrine_migration_versions`
 --
 ALTER TABLE `doctrine_migration_versions`
   ADD PRIMARY KEY (`version`);
 
 --
--- Indexes for table `todo`
+-- Indizes für die Tabelle `status`
 --
-ALTER TABLE `todo`
+ALTER TABLE `status`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Indizes für die Tabelle `todo`
+--
+ALTER TABLE `todo`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_5A0EB6A0AAED72D` (`fk_status_id`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
 --
 
 --
--- AUTO_INCREMENT for table `todo`
+-- AUTO_INCREMENT für Tabelle `status`
+--
+ALTER TABLE `status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT für Tabelle `todo`
 --
 ALTER TABLE `todo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints der exportierten Tabellen
+--
+
+--
+-- Constraints der Tabelle `todo`
+--
+ALTER TABLE `todo`
+  ADD CONSTRAINT `FK_5A0EB6A0AAED72D` FOREIGN KEY (`fk_status_id`) REFERENCES `status` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
